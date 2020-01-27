4b9966a5c4a78db5fd987a85a8b134c115ef26e6
Christophe Painchaud <shellescape@gmail.com>
2020-01-27 22:06:40 +0100

    test again

--- MMOCoreORB/src/server/zone/managers/crafting/labratories/GeneticLabratory.cpp
+++ MMOCoreORB/src/server/zone/managers/crafting/labratories/GeneticLabratory.cpp
@@ -22,7 +22,7 @@ GeneticLabratory::~GeneticLabratory() {
 
 String GeneticLabratory::pickSpecialAttack(String a, String b, String c, String d, String e, int odds, String otherSpecial) {
 	String effectiveSpecial = "defaultattack";
-	printf("BE: Spe attack %s\n", (LPCSTR)a);
+	printf("BE: Spe attack %s\n", a.GetString());
 	// if no special was found in the first passed in slot pick one at random
 	if (a.isEmpty() || a == otherSpecial) {
 		int rand = System::random(3);
