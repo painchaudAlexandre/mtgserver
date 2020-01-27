79e7e9e7ba303dd2b48a80b24688dadd48523b4e
Christophe Painchaud <shellescape@gmail.com>
2020-01-27 21:52:19 +0100

    retest

--- MMOCoreORB/src/server/zone/managers/crafting/labratories/GeneticLabratory.cpp
+++ MMOCoreORB/src/server/zone/managers/crafting/labratories/GeneticLabratory.cpp
@@ -24,6 +24,7 @@ String GeneticLabratory::pickSpecialAttack(String a, String b, String c, String 
 	String effectiveSpecial = "defaultattack";
     printf("BE: Spe attack");
 	cout << "BE: Spe attack " << a << std::endl
+	printf("BE: Spe attack %s\n", (LPCSTR) a)
 	// if no special was found in the first passed in slot pick one at random
 	if (a.isEmpty() || a == otherSpecial) {
 		int rand = System::random(3);
